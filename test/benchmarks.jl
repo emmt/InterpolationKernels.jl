c0ccbc35f73ef01f6849ee2b14b4690dbc3f0e7a
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2021-03-27 12:03:01 +0100

    Benchmark calling the kernels as functions

--- test/benchmarks.jl
+++ test/benchmarks.jl
@@ -2,7 +2,9 @@ module BenchmarkingInterpolationKernels
 
 using BenchmarkTools, Statistics, Printf
 using InterpolationKernels
-using InterpolationKernels: compute_weights, compute_offset_and_weights, brief
+using InterpolationKernels:
+    compute_weights, compute_offset_and_weights,
+    brief, support, infimum, supremum
 
 function compute_offset_and_weights!(y::Array{T,2},
                    ker::Kernel{T,1},
@@ -58,19 +60,85 @@ function compute_offset_and_weights!(y::Array{T,2},
     nothing
 end
 
+function simdmap!(f,
+                  dst::AbstractArray{<:Any,N},
+                  src::AbstractArray{<:Any,N}) where {N}
+    @inbounds @simd for i in eachindex(dst, src)
+        dst[i] = f(src[i])
+    end
+    return dst
+end
+
+number_of_operations(f, ker::Kernel) = number_of_operations(f, typeof(ker))
+number_of_operations(ker::Kernel) = number_of_operations(ker, ker)
+
+# For calling the function on a single value, the number of operations is the
+# maximum possible.  Function compute_offset_and_weights usually adds 3
+# operations compared to compute_weights.
+for (K, n1, n2, n3) in ((BSpline{1},          2,  2,  2),
+                        (BSpline{2},          2,  1,  4),
+                        (BSpline{3},          3,  6,  9),
+                        (BSpline{4},          6, 15, 18), # 5 or 6
+                        (CubicSpline,         6, 15, 18),
+                        (CardinalCubicSpline, 6, 11, 14),
+                        (CatmullRomSpline,    6, 10, 13),)
+    @eval begin
+        number_of_operations(::$K, ::Type{<:$K}) = $n1
+        number_of_operations(::typeof(compute_weights),::Type{<:$K}) = $n2
+        number_of_operations(::typeof(compute_offset_and_weights),::Type{<:$K}) = $n3
+    end
+end
+
 gflops(nops::Real, ns::Real) = (ns, nops/ns)
 
 function runtests(n::Int = 1000; verbose::Bool=false, io::IO=stdout)
     for T in (Float32, Float64)
         x = 200*rand(T, n) .- 100;
-        for (ker, nops) in ((BSpline{1,T}(), 2*n),
-                            (BSpline{2,T}(), 3*n),
-                            (BSpline{3,T}(), 9*n),
-                            (BSpline{4,T}(), 17*n),
-                            (CatmullRomSpline{T}(), 13*n),
-                            (CardinalCubicSpline{T}(-1/2), 14*n),
-                            (MitchellNetravaliSpline{T}(1/3,1/3), 18*n),)
-            print(io, "Tests for $(summary(ker)): ")
+        for ker in (BSpline{1,T}(),
+                    BSpline{2,T}(),
+                    BSpline{3,T}(),
+                    BSpline{4,T}(),
+                    CubicSpline{T}(-1/2,1/6),
+                    CatmullRomSpline{T}(),
+                    CardinalCubicSpline{T}(-1/2),
+                    MitchellNetravaliSpline{T}(1/3,1/3),)
+            nops = number_of_operations(compute_offset_and_weights, ker)*n
+            print(io, "Testing `compute_offset_and_weights` for ",
+                  summary(ker), " ", n, " times: ")
+            flush(io)
+            y = Array{T}(undef, 1 + length(ker), n)
+            b = @benchmark $(compute_offset_and_weights!)($y, $ker, $x)
+            t = b.times
+            @printf(io, "%.3f Gflops\n", nops/minimum(t))
+            if verbose
+                @printf(io, "  - memory: %d allocations, %d bytes\n",
+                        b.allocs, b.memory)
+                @printf(io, "  - minimum time: %7.0f ns, %.3f Gflops\n",
+                        gflops(nops, minimum(t))...)
+                @printf(io, "  - median time:  %7.0f ns, %.3f Gflops\n",
+                        gflops(nops, median(t))...)
+                @printf(io, "  - mean time:    %7.0f ns, %.3f Gflops\n",
+                        gflops(nops, mean(t))...)
+                @printf(io, "  - maximum time: %7.0f ns, %.3f Gflops\n",
+                        gflops(nops, maximum(t))...)
+                println(io)
+            end
+        end
+    end
+    for T in (Float32, Float64)
+        for ker in (BSpline{1,T}(),
+                    BSpline{2,T}(),
+                    BSpline{3,T}(),
+                    BSpline{4,T}(),
+                    CubicSpline{T}(-1/2,1/6),
+                    CatmullRomSpline{T}(),
+                    CardinalCubicSpline{T}(-1/2),
+                    MitchellNetravaliSpline{T}(1/3,1/3),)
+            xmin = infimum(support(ker))
+            xmax = supremum(support(ker))
+            x = (xmax - xmin)*rand(T, n) .+ xmin;
+            nops = number_of_operations(ker)*n
+            print(io, "Testing calling ", summary(ker), " ", n, " times: ")
             flush(io)
             y = Array{T}(undef, 1 + length(ker), n)
             b = @benchmark $(compute_offset_and_weights!)($y, $ker, $x)
